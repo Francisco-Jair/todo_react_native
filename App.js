0298f0629ee45636492effe874ae422be86dde4c
Francisco Jair <fjair123@ufpi.edu.br>
2022-01-25 19:17:57 -0300

    Parte visual completa

--- App.js
+++ App.js
@@ -1,20 +1,131 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+  FlatList,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 
 export default function App() {
+  const [task, setTask] = useState(["jair", "marcos"]);
+  const [newtask, setNewtask] = useState("");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <KeyboardAvoidingView
+        keyboardVerticalOffset={0}
+        behavior="padding"
+        style={{ flex: 1 }}
+        enabled={Platform.OS === "ios"}
+      >
+        <View style={styles.container}>
+          <View style={styles.body}>
+            <FlatList
+              style={styles.flatlist}
+              data={task}
+              keyExtractor={(item) => item.toString()}
+              showsVerticalScrollIndicator={false}
+              renderItem={({ item }) => (
+                <View style={styles.containerView}>
+                  <Text style={styles.texto}>{item}</Text>
+                  <TouchableOpacity>
+                    <MaterialIcons
+                      name="delete-forever"
+                      size={25}
+                      color="#f64c75"
+                    />
+                  </TouchableOpacity>
+                </View>
+              )}
+            />
+            <StatusBar style="auto" />
+          </View>
+
+          <View style={styles.form}>
+            <TextInput
+              style={styles.input}
+              placeholderTextColor="#999"
+              autoCorrect={true}
+              placeholder="Adicione uma tarefa"
+              maxLength={25}
+            />
+            <TouchableOpacity style={styles.botao}>
+              <Ionicons name="ios-add" size={25} color="#FFF" />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    marginTop: 20,
+  },
+  body: {
+    flex: 1,
+  },
+  form: {
+    padding: 0,
+    height: 60,
+    justifyContent: "center",
+    alignSelf: "stretch",
+    flexDirection: "row",
+    paddingTop: 13,
+    borderTopWidth: 1,
+    borderColor: "#eee",
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    backgroundColor: "#eee",
+    borderRadius: 4,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: "#eee",
+  },
+  botao: {
+    height: 40,
+    width: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#1c6cce",
+    borderRadius: 4,
+    marginLeft: 10,
+  },
+  flatlist: {
+    flex: 1,
+    marginTop: 5,
+  },
+  containerView: {
+    marginBottom: 15,
+    padding: 15,
+    borderRadius: 4,
+    backgroundColor: "#eee",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    borderWidth: 1,
+    borderColor: "#eee",
+  },
+  texto: {
+    fontSize: 14,
+    color: "#333",
+    fontWeight: "bold",
+    marginTop: 4,
+    textAlign: "center",
   },
 });
